3c61443305476708dcd2991e760485146b945e0b
khIbrahim <ibrahim84khaled@gmail.com>
2026-07-01 11:37:43 +0100

    refactor(migrations): for proper migrate from scratch

--- database/migrations/2025_09_17_174024_add_tags_and_description_to_categories.php
+++ database/migrations/2025_09_17_174024_add_tags_and_description_to_categories.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('categories', function (Blueprint $table) {
-            $table->text('description')->nullable()->after('slug');
+            $table->text('description')->nullable()->after('sku');
             $table->string('tags')->nullable()->after('description');
         });
     }
